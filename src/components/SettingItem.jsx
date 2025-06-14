0eefb498fef6c82ab9ac836723adc5d127f745cb
katbang <katr164m@stud.kea.dk>
2025-06-14 22:46:32 +0200

    adding setting button

---
+++ src/components/SettingItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+export default function SettingItem({ type, image, imageOnHover, text, altText }) {
+  const [itemState, setItemState] = useState("default");
+  return (
+    <button
+      className={`w-[290px] h-[260px] bg-main-color cursor-pointer border-4 border-solid ${
+        itemState === "default" ? "border-main-color" : "border-primary"
+      } rounded-lg flex flex-col justify-center items-center px-4 gap-3`}
+      onMouseOver={() => setItemState("hover")}
+      onMouseOut={() => setItemState("default")}
+    >
+      <img src={itemState === "default" ? image : imageOnHover} alt={altText} />
+      <p className="text-primary text-3xl font-bold pt-4">
+        {text}
+        {type === "toggle" && <span>til</span>}
+        {type === "toggle2" && <span>til</span>}
+      </p>
+    </button>
+  );
+}
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,13 @@
 import Menu from "../components/Menu";
+import SettingItem from "../components/SettingItem";
 const Settings = () => {
   return (
-    <main className="mx-20">
+    <main className="bg-alt-color h-screen">
       <Menu />
+      <h1 className="text-font-color font-bold text-3xl text-center py-16">Indstillinger</h1>
+      <div className="flex justify-center">
+        <SettingItem text="Synsvanskeligheder" image="/assets/eye-icon.svg" imageOnHover="/assets/eye-icon-filled.svg" altText="eye icon" type="pop-up" />
+      </div>
     </main>
   );
 };
