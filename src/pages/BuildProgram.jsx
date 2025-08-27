d201b96f6cc76b5651a4fc156997f57278c5f6b2
simonj1704 <simonj1704@gmail.com>
2025-08-27 19:49:11 +0200

    removed unused variable

--- src/pages/BuildProgram.jsx
+++ src/pages/BuildProgram.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect, use } from "react";
+import { useRef, useState, useEffect } from "react";
 import TaskList from "../components/TaskList";
 import Menu from "../components/Menu";
 import ProgramModal from "../components/modals/ProgramModal";
