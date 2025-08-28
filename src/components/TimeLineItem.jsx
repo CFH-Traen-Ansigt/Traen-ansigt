843fc5508aabe2c18c9c932d774ba1b369be20f6
simonj1704 <simo915m@stud.kea.dk>
2025-08-28 12:07:42 +0200

    Added background color to text in about us page

--- src/components/TimeLineItem.jsx
+++ src/components/TimeLineItem.jsx
@@ -2,7 +2,7 @@ export default function TimelineItem({ icon, text, position, iconWidth = "w-[70%
   return (
     <div className={`absolute ${position} flex flex-col gap-2 `}>
       <img src={`assets/${icon}`} className={`m-auto ${iconWidth}`} alt="timeline icon" />
-      <p className="text-alt-color text-center font-light">{text}</p>
+      <p className="text-alt-color text-center font-light bg-primary">{text}</p>
     </div>
   );
 }
